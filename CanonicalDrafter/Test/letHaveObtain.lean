12e5548b8a270cc3355d7854eea986db4c042d00
FrederickPu <pufrederick51@gmail.com>
2025-09-02 02:51:28 +0000

    fixed tests

---
+++ CanonicalDrafter/Test/letHaveObtain.lean
@@ -0,0 +1,11 @@
+example : 2 + 2 = 4 := by
+  -- fall through cases
+  let ⟨i, hi⟩ : Fin 3 := 0
+  obtain ⟨a, b⟩ : 2 = 2 ∧ 3 = 3 := by
+    apply And.intro
+    rfl
+    rfl
+  -- non fall through cases
+  have bruh : ∃ x : Nat, x = x := ⟨5, rfl⟩
+  let ⟨test1, test2⟩ := bruh
+  rfl
