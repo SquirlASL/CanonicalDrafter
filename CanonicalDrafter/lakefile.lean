e40187b7bc7e0018627306228faeac95403cdf9a
FrederickPu <pufrederick51@gmail.com>
2025-09-24 15:40:26 +0000

    upgraded lean version

--- CanonicalDrafter/lakefile.lean
+++ CanonicalDrafter/lakefile.lean
@@ -7,12 +7,12 @@ package canonicaldrafter where
   supportInterpreter := true
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.21.0"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.22.0"
 
 require Canonical from git
-  "https://github.com/chasenorman/CanonicalLean" @ "v4.21.0"
+  "https://github.com/chasenorman/CanonicalLean" @ "v4.22.0"
 
-require REPL from git "https://github.com/leanprover-community/repl" @ "v4.21.0"
+require REPL from git "https://github.com/leanprover-community/repl" @ "v4.22.0"
 
 lean_lib CanonicalDrafter where
   -- Your library code here if needed
@@ -21,4 +21,8 @@ lean_lib Test where
 
 lean_exe test {
   root := `Test
+}
+
+lean_exe extract_data {
+  root := `ExtractData
 }
