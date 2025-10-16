28c3de11c16cf6a190b1fc13383b87c420b2601f
FrederickPu <pufrederick51@gmail.com>
2025-10-16 16:35:11 -0400

    sketched llm drafter (note : a lot of code duplication)

---
+++ CanonicalDrafter/LLMDrafter.lean
@@ -0,0 +1,128 @@
+import Lean
+import Aesop
+import LLMlean
+
+open Lean Meta Elab Term Expr Meta Tactic
+
+namespace Typewriter
+
+def elabStringAsExpr (code : String) : TermElabM Expr := do
+  -- parse the string as a syntax tree
+  let stx := (Parser.runParserCategory (← getEnv) `term code).toOption.get!
+  -- elaborate it into an expression
+  withoutErrToSorry do
+    elabTermAndSynthesize stx none
+
+def printForce (s : String) : IO Unit := do
+  let handle ← IO.FS.Handle.mk "output.txt" IO.FS.Mode.append
+  handle.putStrLn s
+  handle.flush
+
+unsafe def drafter (goal : MVarId) (draftIn draftOut draftErr : Std.Channel.Sync String): TermElabM (Option Expr) := do
+  -- if we exceed bounds then return none
+  -- let goal ← introNP goal (typeArity1 goal.getType)
+  -- exposeNames?
+  goal.withContext do
+    draftOut.send (← Meta.ppGoal goal).pretty
+    let duplicate := ← mkFreshExprMVar (← goal.getType)
+    let remaining ←
+      try
+        let config ← Aesop.Frontend.TacticConfig.parse (← `(tactic| aesop)) duplicate.mvarId!
+        let (remaining, stats) ← Aesop.search duplicate.mvarId! (← config.getRuleSet duplicate.mvarId!) config.options config.simpConfig
+            config.simpConfigSyntax?
+        pure remaining
+      catch e =>
+        draftErr.send (← e.toMessageData.format).pretty
+        pure #[duplicate.mvarId!]
+
+    if remaining.isEmpty then
+      return ← instantiateMVars duplicate -- return the `aesop` syntax
+
+    -- attempt closers (in parallel)
+    let type ← draftIn.recv
+    let (name, draft) : String × String := ("test", type) -- ← letDrafter input
+    -- printForce "wee"
+    try
+      let output ← elabStringAsExpr draft
+      let subgoal ← mkFreshExprMVar output
+      let (x, newGoal) ← goal.note name.toName subgoal
+      if let some value ← drafter subgoal.mvarId! draftIn draftOut draftErr then
+        if let some body ← drafter newGoal draftIn draftOut draftErr then
+          return some (.letE name.toName output value
+            (body.abstract #[.fvar x]) false)
+    catch e =>
+      draftErr.send (← e.toMessageData.format).pretty
+    -- printForce "womp"
+    return none
+
+structure RpcChannel where
+  channel : Std.Channel.Sync String
+deriving TypeName
+
+open Server RequestM
+
+structure SendParams where
+  pos : Lsp.Position
+  text : String
+  channel : Server.WithRpcRef RpcChannel
+deriving Server.RpcEncodable
+
+@[server_rpc_method]
+def send (params : SendParams) : RequestM (RequestTask Json) := do
+  withWaitFindSnapAtPos params.pos fun snap => runTermElabM snap do
+    params.channel.val.channel.send params.text
+    return default
+
+structure RecvParams where
+  pos : Lsp.Position
+  channel : Server.WithRpcRef RpcChannel
+deriving Server.RpcEncodable
+
+structure RpcString where
+  response : String
+deriving Lean.Server.RpcEncodable
+
+@[server_rpc_method]
+def recv (params : RecvParams) : RequestM (RequestTask RpcString) := do
+  withWaitFindSnapAtPos params.pos fun snap => runTermElabM snap do
+    printForce "recv"
+    let temp ← params.channel.val.channel.recv
+    printForce temp
+    return ⟨temp⟩
+
+@[widget_module]
+def refineWidget : Widget.Module where
+  javascript := include_str "TypeDrafter.js"
+
+open Lean Meta Elab Tactic
+
+unsafe def elabTypeWriteImpl : TacticM Unit := do
+  let draftIn ← Std.Channel.Sync.new (α := String)
+  let draftOut ← Std.Channel.Sync.new (α := String)
+  let draftErr ← Std.Channel.Sync.new (α := String)
+
+  let goal ← getMainGoal
+  goal.admit
+  let duplicate := (← mkFreshExprMVar (← goal.getType)).mvarId!
+  let ctxCore : Core.Context ← monadLift (read : CoreM Core.Context)
+  let sCore : Core.State ← monadLift (get : CoreM Core.State)
+  let ctxMeta : Meta.Context ← monadLift (read : MetaM _)
+  let sMeta : Meta.State ← monadLift (get : MetaM _)
+  let ctx ← readThe Term.Context
+  let x ← IO.asTask (Lean.Elab.Term.TermElabM.toIO (drafter duplicate draftIn draftOut draftErr) ctxCore sCore ctxMeta sMeta ctx {})
+
+  -- llm loop
+  while true do
+    let goalString ← draftOut.recv
+    let draft ← LLMlean.Config.API.generate { model := "gemini", baseUrl := "i dunno" } [goalString] { numSamples := 1 }
+    draftIn.send draft[0]!.1
+
+@[implemented_by elabTypeWriteImpl]
+opaque elabTypeWrite : TacticM Unit
+
+
+elab "typewrite" : tactic => elabTypeWrite
+
+def x : 0 = 0 := by grind
+example : 0 + n = n ^ n := by
+  typewrite
--- CanonicalDrafter/lakefile.lean
+++ CanonicalDrafter/lakefile.lean
@@ -7,12 +7,15 @@ package canonicaldrafter where
   supportInterpreter := true
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.22.0"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.23.0"
 
 require Canonical from git
-  "https://github.com/chasenorman/CanonicalLean" @ "v4.22.0"
+  "https://github.com/chasenorman/CanonicalLean" @ "v4.23.0"
 
-require REPL from git "https://github.com/leanprover-community/repl" @ "v4.22.0"
+require REPL from git "https://github.com/leanprover-community/repl" @ "2f8073af0a5e3a141fee075652790a2c19132516"
+
+require llmlean from git
+  "https://github.com/FrederickPu/llmlean.git"  @ "unify-generation"
 
 lean_lib CanonicalDrafter where
   -- Your library code here if needed
